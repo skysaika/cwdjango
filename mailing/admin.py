d5fac89bae39c4b0588714f8fa7eb611e7b9d62e
saysuu <skysaika@yandex.ru>
2024-02-26 20:24:20 +0300

    register admin for MailingMessage, MailingList, MailingLog

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
 
-from mailing.models import Client
+from mailing.models import Client, MailingMessage, MailingList, MailingLog
 
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    """Зарегистрировала модель клиента"""
-    list_display = ('email', 'first_name', 'last_name', 'comment')
-    search_fields = ('email', 'first_name', 'last_name')
-    list_filter = ('email',)
+    """Админка модели клиента"""
+    list_display = ('email', 'first_name', 'last_name', 'comment',)
 
+
+@admin.register(MailingMessage)
+class MailingMessageAdmin(admin.ModelAdmin):
+    """Админка модели сообщения"""
+    list_display = ('subject', 'body',)
+
+
+@admin.register(MailingList)
+class MailingListAdmin(admin.ModelAdmin):
+    """Админка модели рассылки"""
+    list_display = ('mail_start', 'mail_end', 'mail_period', 'mail_status',)
+
+
+@admin.register(MailingLog)
+class MailingLogAdmin(admin.ModelAdmin):
+    """Админка модели логов рассылок"""
+    list_display = ('last_attempt_time', 'attempt_status', 'server_response',)
