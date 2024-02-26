a932cb76eebcbf29976dc41d7280ff03e1b21446
saysuu <skysaika@yandex.ru>
2024-02-26 16:25:08 +0300

    add config/urls to mailing app

---
+++ mailing/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from mailing.apps import MailingConfig
+from mailing.views import IndexView
+
+app_name = MailingConfig.name
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),  # главная страница
+]
