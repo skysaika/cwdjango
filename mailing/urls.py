27cea68712418c857fe92848f5da48b8870a85d0
saysuu <skysaika@yandex.ru>
2024-02-26 16:43:02 +0300

    add contact.html, urls, view and menu in inc_main_menu

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from mailing.apps import MailingConfig
-from mailing.views import IndexView
+from mailing.views import IndexView, contact
 
 app_name = MailingConfig.name
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),  # главная страница
+    path('contact/', contact, name='contact'),  # путь до страницы контактов FBV
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -7,3 +7,15 @@ class IndexView(TemplateView):
     template_name = 'mailing/index.html'
 
 
+def contact(request):
+    """Контроллер страницы контактов на FBV"""
+    # метод POST - получение данных, не забудь вернуть context
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'name:{name}, email:{email}, {message}')
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'mailing/contact.html', context)
