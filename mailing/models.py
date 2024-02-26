06ff3591e08b0d4a74b90ae51d8820f2d6287557
saysuu <skysaika@yandex.ru>
2024-02-26 16:57:17 +0300

    base migrate, create model Client, makemigrations, migrate

--- mailing/models.py
+++ mailing/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Client(models.Model):
+    """Модель клиента"""
+    email = models.EmailField(verbose_name='email')
+    first_name = models.CharField(max_length=100, verbose_name='имя')
+    last_name = models.CharField(max_length=100, verbose_name='фамилия')
+    comment = models.CharField(max_length=255, verbose_name='комментарий', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'клиент рассылки'
+        verbose_name_plural = 'клиенты рассылки'
+        ordering = ('email',)
+
