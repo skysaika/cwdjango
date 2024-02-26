fcc626f51b6c45af6367515405ba01783fe2c192
saysuu <skysaika@yandex.ru>
2024-02-26 16:26:38 +0300

    add urls and view IndexView

--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    """Представление главной страницы."""
+    template_name = 'mailing/index.html'
 
 
