f8f21ac7932e0d7ae4cb1bde68533df970ebb899
saysuu <skysaika@yandex.ru>
2024-03-03 20:02:07 +0300

    add class StyleFormMixin in forms.py

--- mailing/forms.py
+++ mailing/forms.py
@@ -1,9 +1,21 @@
 from django import forms
+from django.forms import CheckboxInput
 
 from mailing.models import Client
 
 
-class ClientForm(forms.ModelForm):
+class StyleFormMixin:
+    """Миксин для стилизации форм"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, CheckboxInput):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+class ClientForm(StyleFormMixin, forms.ModelForm):
     """Форма создания клиента"""
     class Meta:
         model = Client
