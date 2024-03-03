ea70bf23d378b7c122801185d45bf32757cf66e7
saysuu <skysaika@yandex.ru>
2024-03-03 21:43:12 +0300

    create User model, add AUTH_USER_MODEL='users.User'in settings.py, makemigrations, migrate

--- config/settings.py
+++ config/settings.py
@@ -142,8 +142,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# AUTH_USER_MODEL = 'users.User'
-#
+AUTH_USER_MODEL = 'users.User'
+
 # LOGIN_REDIRECT_URL = '/'
 # LOGOUT_REDIRECT_URL = '/'
 
--- users/models.py
+++ users/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from mailing.models import NULLABLE
+
+
+class User(AbstractUser):
+    """Модель пользователя"""
+    username = None  # поле username удалится
+    # заменим способ авторизации через email, который нужно переопределить:
+    email = models.EmailField(unique=True, verbose_name='почта')  # поле уникальное
+
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
+
+    is_active = models.BooleanField(default=False, verbose_name='статус активности')
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('is_active',)
+
