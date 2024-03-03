6020b4f906529cea7c831437612e468960e4c320
saysuu <skysaika@yandex.ru>
2024-03-03 13:45:25 +0300

    create ClientCreateView, ClientListView, ClientDetailView, path in urls, templates:client_form,client_list, client_detail

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
 from mailing.apps import MailingConfig
-from mailing.views import IndexView, contact
+from mailing.views import IndexView, contact, ClientCreateView, ClientListView, ClientDetailView
 
 app_name = MailingConfig.name
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),  # главная страница
+
+    path('create_client/', ClientCreateView.as_view(), name='create_client'),  # создание клиента
+    path('clients/', ClientListView.as_view(), name='clients'),  # список клиентов
+    path('client_view/<int:pk>/', ClientDetailView.as_view(), name='client_view'),  # просмотр клиента
+
+
     path('contact/', contact, name='contact'),  # путь до страницы контактов FBV
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView, ListView
 
-from mailing.models import MailingList
+from mailing.models import MailingList, Client
 
 
 class IndexView(TemplateView):
@@ -11,6 +11,37 @@ class IndexView(TemplateView):
         'title': 'Главная страница',
         'object_list': MailingList.objects.all()
     }
+
+
+class ClientCreateView(CreateView):
+    """Создание клиента"""
+    model = Client
+    template_name = 'mailing/client_form.html'
+    fields = ['email', 'first_name', 'last_name', 'comment']
+    extra_context = {
+        'title': 'Создание клиента'
+    }
+
+
+class ClientListView(ListView):
+    """Просмотр списка клиентов"""
+    model = Client
+    template_name = 'mailing/client_list.html'
+    extra_context = {
+        'title': 'Список клиентов'
+    }
+
+
+class ClientDetailView(TemplateView):
+    """Просмотр одного клиента"""
+    model = Client
+    template_name = 'mailing/client_detail.html'
+    extra_context = {
+        'title': 'Просмотр клиента'
+    }
+
+
+
 
 
 def contact(request):
