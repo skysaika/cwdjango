b0306597d86a9c64bc0637bb299f378d60f8a6c7
saysuu <skysaika@yandex.ru>
2024-03-03 16:12:16 +0300

    changed urls client_list, client_detail

--- mailing/urls.py
+++ mailing/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),  # главная страница
 
     path('create_client/', ClientCreateView.as_view(), name='create_client'),  # создание клиента
-    path('clients/', ClientListView.as_view(), name='clients'),  # список клиентов
-    path('client_view/<int:pk>/', ClientDetailView.as_view(), name='client_view'),  # просмотр клиента
+    path('client_list/', ClientListView.as_view(), name='client_list'),  # список клиентов
+    path('client_detail/<int:pk>/', ClientDetailView.as_view(), name='client_detail'),  # просмотр клиента
 
 
     path('contact/', contact, name='contact'),  # путь до страницы контактов FBV
