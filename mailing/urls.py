e9943ac624c02cd3d7a6ceb2c21d42c5f30e367c
saysuu <skysaika@yandex.ru>
2024-03-03 16:56:22 +0300

    just some change: base.html,index.html, urls.py

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from mailing.apps import MailingConfig
-from mailing.views import IndexView, contact, ClientCreateView, ClientListView, ClientDetailView
+from mailing.views import IndexView, contact, ClientCreateView, ClientListView, ClientDetailView, ClientUpdateView
 
 app_name = MailingConfig.name
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),  # главная страница
 
     path('create_client/', ClientCreateView.as_view(), name='create_client'),  # создание клиента
+    path('client_edit/<int:pk>/', ClientUpdateView.as_view(), name='client_edit'),  # редактирование клиента
     path('client_list/', ClientListView.as_view(), name='client_list'),  # список клиентов
     path('client_detail/<int:pk>/', ClientDetailView.as_view(), name='client_detail'),  # просмотр клиента
 
